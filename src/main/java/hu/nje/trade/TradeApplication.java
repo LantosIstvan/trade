c7c30bececa3a77d899a5a72c29acc6af9f0fd37
István Lantos <lantosistvan89@gmail.com>
2025-11-05 17:29:46 +0100

    Oanda ctx elérhetővé téve Controller-ek számára

--- src/main/java/hu/nje/trade/TradeApplication.java
+++ src/main/java/hu/nje/trade/TradeApplication.java
@@ -1,13 +1,22 @@
 package hu.nje.trade;
 
+import com.oanda.v20.Context;
+import com.oanda.v20.ContextBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TradeApplication {
+    @Bean
+    public Context oandaContext() {
+        return new ContextBuilder(Config.URL)
+            .setToken(Config.TOKEN)
+            .setApplication("TradeApp")
+            .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TradeApplication.class, args);
     }
-
 }
--- src/main/java/hu/nje/trade/controller/FooldalController.java
+++ src/main/java/hu/nje/trade/controller/FooldalController.java
@@ -1,15 +1,23 @@
 package hu.nje.trade.controller;
 
+import com.oanda.v20.Context;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FooldalController {
+    private final Context ctx;
+
+    public FooldalController(Context ctx) {
+        this.ctx = ctx;
+    }
+
     @GetMapping("/")
     public String templateTest(Model model) {
         model.addAttribute("pageTitle", "Főoldal");
         model.addAttribute("welcomeMessage", "A new model for open collaboration");
+        // Oanda ctx használható itt, más menüpontokban lehet hasznos
         return "fooldal";
     }
 }
