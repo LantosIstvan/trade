370217519d7afc9ac705300e94d0968802a5826a
Fityó Györgyi <gyorgyi.fityo@gmail.com>
2025-11-11 19:38:33 +0100

    Zár menü kész

---
+++ src/main/java/hu/nje/trade/controller/ZarController.java
@@ -0,0 +1,50 @@
+package hu.nje.trade.controller;
+
+import com.oanda.v20.Context;
+import com.oanda.v20.trade.TradeCloseRequest;
+import com.oanda.v20.trade.TradeCloseResponse;
+import com.oanda.v20.trade.TradeSpecifier;
+import hu.nje.trade.Config;
+import hu.nje.trade.dto.ZarDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ZarController {
+    private final Context ctx;
+
+    public ZarController(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @GetMapping("/forex-zar")
+    public String getZar(Model model) {
+        model.addAttribute("param", new ZarDTO());
+
+        String title = "Zárás menü";
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("heroMessage", title);
+        return "zar_get";
+    }
+
+    @PostMapping("/forex-zar")
+    public String postZar(@ModelAttribute ZarDTO zarDTO, Model model) {
+        String tradeId = zarDTO.getTradeId() + "";
+        try {
+            ctx.trade.close(new TradeCloseRequest(Config.ACCOUNTID, new TradeSpecifier(tradeId)));
+            model.addAttribute("tradeId", tradeId);
+        } catch (Exception e) {
+            model.addAttribute("error", "Hiba történt az OANDA API lekérése során.");
+            e.printStackTrace();
+        }
+
+        String title = "Zárás menü | Eredmény";
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("heroMessage", title);
+        return "zar_post";
+    }
+
+}
---
+++ src/main/java/hu/nje/trade/dto/ZarDTO.java
@@ -0,0 +1,13 @@
+package hu.nje.trade.dto;
+
+public class ZarDTO {
+    private int tradeId;
+
+    public int getTradeId() {
+        return tradeId;
+    }
+
+    public void setTradeId(int tradeId) {
+        this.tradeId = tradeId;
+    }
+}
